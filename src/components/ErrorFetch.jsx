6349d675fd7589c4589e49a6b3f8412a8dae35cb
Andres1717 <mariaerq10@gmail.com>
2022-12-05 02:13:40 -0500

    Agregando Hover al boton y estilos

--- src/components/ErrorFetch.jsx
+++ src/components/ErrorFetch.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ErrorFetch = () => {
   return (
-    <h1>+ Solo debes ingresar numeros desde el 1 al 126</h1>
+    <h1>❌ Solo puedes ingresar numeros desde el 1 al 126 ❌</h1>
   )
 }
 
